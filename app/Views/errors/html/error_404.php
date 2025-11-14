0c88b3889380523614ff96c22cf802c037f5afac
Muhammad Fattahillah. Mz <fattahillahmuhammad48@gmail.com>
2025-11-14 13:50:04 +0700

    Halaman 404 sudah dibuat

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,84 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title><?= lang('Errors.pageNotFound') ?></title>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Tidak Ditemukan - 404 Not Found</title>
     <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif
         }
+
         body {
-            height: 100%;
-            background: #fafafa;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            color: #777;
-            font-weight: 300;
+            width: 100vw;
+            height: 100vh;
+            background-image: url(/assets/images/error-pages/background.svg);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center
         }
-        h1 {
-            font-weight: lighter;
-            letter-spacing: normal;
-            font-size: 3rem;
-            margin-top: 0;
-            margin-bottom: 0;
-            color: #222;
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 16px
         }
-        .wrap {
-            max-width: 1024px;
-            margin: 5rem auto;
-            padding: 2rem;
-            background: #fff;
-            text-align: center;
-            border: 1px solid #efefef;
-            border-radius: 0.5rem;
-            position: relative;
+
+        .container figure.not-found-img img {
+            width: 500px;
+            height: 279px;
+            filter: drop-shadow(3px 3px 2px rgb(0 0 0 / .45))
         }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
+
+        .container h1 {
+            font-size: 1.5rem;
+            color: #fff;
+            text-shadow: 1.5px 2px rgb(0 0 0 / .5)
         }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
+
+        .container a.home-link {
+            padding: 8px 18px;
+            background-color: oklch(55.8% .288 302.321);
+            text-decoration: none;
+            color: #fff;
+            border-radius: 4px;
+            transition: .15s ease-in;
+            border: 2px solid oklch(55.8% .288 302.321);
+            margin-block-start: 8px;
+            font-size: .95rem;
+            font-weight: 700;
+            letter-spacing: .055em;
+            font-family: 'Segoe UI'
         }
-        p {
-            margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
+
+        .container a:hover {
+            background-color: oklch(49.6% .265 301.924)
         }
     </style>
 </head>
-<body>
-    <div class="wrap">
-        <h1>404</h1>
 
-        <p>
-            <?php if (ENVIRONMENT !== 'production') : ?>
-                <?= nl2br(esc($message)) ?>
-            <?php else : ?>
-                <?= lang('Errors.sorryCannotFind') ?>
-            <?php endif; ?>
-        </p>
+<body>
+    <div class="container">
+        <h1>Oops! Halaman yang kamu cari... sepertinya belum ada disemesta ini!</h1>
+        <figure class="not-found-img">
+            <img src="/assets/images/error-pages/404-text.webp" alt="404 Not Found Image" />
+        </figure>
+        <a href="/" class="home-link">KEMBALI KE ASAL</a>
     </div>
 </body>
+
 </html>
